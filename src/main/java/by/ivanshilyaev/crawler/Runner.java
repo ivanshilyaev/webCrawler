36aa80a4664df6c1aaff47a9bd99848fa7c486f3
Ivan Shilyaev <shilyaev1433@gmail.com>
2020-07-04 16:16:48 +0300

    For now application takes first 10 pages in google search and on each page searches for given words (attributes)

--- src/main/java/by/ivanshilyaev/crawler/Runner.java
+++ src/main/java/by/ivanshilyaev/crawler/Runner.java
@@ -6,55 +6,55 @@ import com.google.api.services.customsearch.Customsearch;
 import com.google.api.services.customsearch.CustomsearchRequestInitializer;
 import com.google.api.services.customsearch.model.Result;
 import com.google.api.services.customsearch.model.Search;
-import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.security.GeneralSecurityException;
 
 public class Runner {
+    private static final String SEARCH_ENGINE_ID = "009675484660055542115:3viexnmtvvq";
+    private static final String API_KEY = "AIzaSyChq7hCghn5jL6Ydmg2V0IVEzmGMa9NhUo";
+    private static String searchQuery;
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private static String[] attrs = {"Spring", "Boot", "Java", "and"};
 
     public static void main(String[] args) {
         try {
-            String blogUrl = "https://spring.io/blog";
-            Document doc = Jsoup.connect(blogUrl).get();
-            String text = doc.body().text();
-            for (String attr : attrs) {
-                System.out.println(attr + ": " + StringUtils.countMatches(text, attr));
-            }
-
-            String searchQuery = "instagram"; // The query to search
-            String cx = "009675484660055542115:3viexnmtvvq"; // Search engine id
+            searchQuery = "java spring boot";
 
             // Instance Custom search
-            Customsearch cs = new Customsearch.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), null)
-                    .setApplicationName("MyApplication")
-                    .setGoogleClientRequestInitializer(new CustomsearchRequestInitializer("AIzaSyChq7hCghn5jL6Ydmg2V0IVEzmGMa9NhUo")) // API key
+            Customsearch cs = new Customsearch.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                    JacksonFactory.getDefaultInstance(), null)
+                    .setApplicationName("webCrawler")
+                    .setGoogleClientRequestInitializer(new CustomsearchRequestInitializer(API_KEY))
                     .build();
 
             //Set search parameter
-            Customsearch.Cse.List list = cs.cse().list(searchQuery).setCx(cx);
+            Customsearch.Cse.List list = cs.cse().list(searchQuery).setCx(SEARCH_ENGINE_ID);
 
             //Execute search
             Search result = list.execute();
             if (result.getItems() != null) {
-                for (Result ri : result.getItems()) {
-                    // Get title, link, body etc. from search
-                    System.out.println(ri.getTitle() + ", " + ri.getLink());
+                for (Result resultItem : result.getItems()) {
+                    System.out.println(resultItem.getTitle() + ": " + resultItem.getLink());
+                    System.out.println("Result:");
+                    String url = resultItem.getLink();
+                    Document doc = Jsoup.connect(url).get();
+                    String text = doc.body().text();
+                    for (String attr : attrs) {
+                        System.out.println(attr + ": " + StringUtils.countMatches(text, attr));
+                    }
+                    System.out.println("\n");
                 }
             }
         } catch (IOException | GeneralSecurityException e) {
-            e.printStackTrace();
+            LOGGER.error("Search failure");
         }
     }
 }
